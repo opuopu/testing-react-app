e8496968db1ad196d6e8232621c636a6515064f3
opuopu <nurmdopu428@gmail.com>
2023-11-08 19:16:05 +0600

    done inititalize react app and setup testing boilerplate

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+/* eslint-disable react/no-unknown-property */
+/// <reference types="vitest"/>
+/// <reference types="vite/client"/>
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./src/setupTest.js"],
+  },
+});
